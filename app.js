15e54ce0c59510ed71228c6560d31ec002e881e0
jorabonjorno <rus74.wisp@gmail.com>
2022-07-01 10:23:59 +0300

    Release 0

---
+++ app.js
@@ -0,0 +1,15 @@
+require('@babel/register');
+
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const app = express();
+require('dotenv').config()
+const PORT = process.env.PORT ?? 3000;
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => {
+    console.log(`server started PORT: ${PORT}`);
+});
