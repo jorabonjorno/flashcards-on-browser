acfe928d4eaa63bab2b1d66acab8f929bd775934
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-07-01 11:57:50 +0300

    layout fix

--- view/Layout.jsx
+++ view/Layout.jsx
@@ -16,16 +16,15 @@ module.exports = function Layout({ title, children }) {
       <body>
         <nav className="navbar navbar-expand-lg bg-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">FlashCards</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon" />
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div className="navbar-nav">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                <a className="nav-link" href="#">Features</a>
-                <a className="nav-link" href="#">Pricing</a>
-                <a className="nav-link disabled">Disabled</a>
+                <a className="nav-link" href="#">Заглушка</a>
+                <a className="nav-link" href="#">Заглушка</a>
+                <a className="nav-link" href="#">Заглушка</a>
               </div>
             </div>
           </div>
