8d122d7feb9ff92084387ed6219317127013f9c8
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-07-01 11:54:22 +0300

    layout fix

--- view/Layout.jsx
+++ view/Layout.jsx
@@ -4,12 +4,34 @@ module.exports = function Layout({ title, children }) {
   return (
     <html lang="en">
       <head>
-        <meta chaSet="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="	https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="/css/main.css" />
+        <script src="/js/client.js" defer />
         <title>{title}</title>
       </head>
-      <body />
+      <body>
+        <nav className="navbar navbar-expand-lg bg-light">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link" href="#">Features</a>
+                <a className="nav-link" href="#">Pricing</a>
+                <a className="nav-link disabled">Disabled</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+        {children}
+      </body>
     </html>
   );
 };
---
+++ view/Register.jsx
@@ -0,0 +1,10 @@
+const Layout = require('./Layout');
+const React = require('react');
+
+mpdule.exports = function Register({title}) {
+  return (
+    <Layout title = {title}>
+      
+    </Layout>
+  )
+}
