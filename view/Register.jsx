31513683b8270a49efe3a28cfe4883cabebe210a
Nikita Krivosheyev <nik.krivosheyev@gmail.com>
2022-07-01 12:15:12 +0300

    register added

--- view/Register.jsx
+++ view/Register.jsx
@@ -3,8 +3,23 @@ const React = require('react');
 
 mpdule.exports = function Register({title}) {
   return (
-    <Layout title = {title}>
-      
+    <Layout title ="Registration">
+      <form>
+        <div class="mb-3">
+          <label for="exampleInputLogin1" class="form-label">Логин</label>
+          <input type="text" class="form-control" id="exampleInputLogin1">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Пароль</label>
+          <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label">Email</label>
+          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+          <div id="emailHelp" class="form-text">Ваши данные не будут переданы третьим лицам</div>
+        </div>
+        <button type="submit" class="btn btn-primary">Подтвердить</button>
+      </form>
     </Layout>
-  )
-}
+  );
+};
